585c9b1e22a01acabe050e4914525e730742643f
mazenhaouari40 <mazenhaouari97@gmail.com>
2024-08-19 18:34:14 +0200

    update socketurl

--- src/app/Service/web-socket-service.service.ts
+++ src/app/Service/web-socket-service.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { WebSocketSubject } from 'rxjs/webSocket';
 import { Observable } from 'rxjs';
+import { global_var } from 'src/environments/global';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
   private socket$: WebSocketSubject<any>;
-
+  private socketurl = global_var.socketapi ; 
   constructor() {
-    this.socket$ = new WebSocketSubject('ws://localhost:8081/ws/notifications');
+    // this.socket$ = new WebSocketSubject('ws://localhost:8081/ws/notifications');
+    this.socket$ = new WebSocketSubject('ws://'+this.socketurl +'/ws/notifications');
+
   }
 
   getMessages(): Observable<any> {
--- src/environments/global.ts
+++ src/environments/global.ts
@@ -1,4 +1,7 @@
 export const global_var = {
-    apiurl : 'https://backend-jenkins.onrender.com'
-    // apiurl : 'http://localhost:8081'
+    apiurl : 'https://backend-jenkins.onrender.com',
+    socketapi : 'backend-jenkins.onrender.com'
+
+    // apiurl : 'http://localhost:8081',
+    // socketapi : 'localhost:8081'
   };
